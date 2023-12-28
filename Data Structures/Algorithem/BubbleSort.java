14dccb12407767ce9e8a68c114d7e4fbda2bedf2
Yasith <yasithnawodya9@gmail.com>
2023-12-28 06:22:18 +0530

    algorithem start

---
+++ Data Structures/Algorithem/BubbleSort.java
@@ -0,0 +1,35 @@
+package Algorithem;
+
+
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+    public static void display(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print("|"+arr[i]+"|");
+        }
+    }
+    public static void main(String[] args) {
+        
+        int arr[]={40,20,30,50,10};
+        System.out.println("Old Array");
+        display(arr);
+        System.out.println();
+        System.out.println("After Bubble Sort");
+        bubbleSort(arr);
+        display(arr);
+
+    }
+}
---
+++ Data Structures/Algorithem/SelectionSort.java
@@ -0,0 +1,39 @@
+package Algorithem;
+
+public class SelectionSort {
+    
+    public static void selectionSort(int arr[]){
+
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+    }
+
+
+
+    public static void display(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print("|"+arr[i]+"|");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={50,12,87,02,46,86,23,16};
+        System.out.println("Old Array");
+        display(arr);
+        System.out.println();
+        System.out.println("After Selection Sort");
+        selectionSort(arr);
+        display(arr);
+    }
+}
