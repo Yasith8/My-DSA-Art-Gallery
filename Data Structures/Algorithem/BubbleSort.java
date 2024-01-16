4fabf134b808912f1d601aeedb8b32c358e11eb0
Yasith <yasithnawodya9@gmail.com>
2024-01-16 18:04:07 +0530

    bubble sort

--- Data Structures/Algorithem/BubbleSort.java
+++ Data Structures/Algorithem/BubbleSort.java
@@ -4,9 +4,9 @@ package Algorithem;
 public class BubbleSort {
 
     public static void bubbleSort(int arr[]){
-        int n=arr.length;
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-1;j++){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j=0;j<n-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
@@ -23,7 +23,7 @@ public class BubbleSort {
     }
     public static void main(String[] args) {
         
-        int arr[]={40,20,30,50,10};
+        int arr[]={3,5,4,7,2,1};
         System.out.println("Old Array");
         display(arr);
         System.out.println();
